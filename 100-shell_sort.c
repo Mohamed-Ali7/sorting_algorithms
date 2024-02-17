b4ff218a2bfafd673de89f9f28a4f899c19ebb32
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-17 12:27:27 +0200

    Updating 100-shell_sort.c file

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,12 +12,11 @@
 void shell_sort(int *array, size_t size)
 {
 	int j, temp = 0;
-	size_t i, gap = 1;
+	size_t i, gap = 0;
 
-	if (array == NULL || size <= 0)
+	if (!array || size <= 0)
 		return;
-
-	while (gap < size / 3)
+	while (gap <= size / 3)
 		gap = gap * 3 + 1;
 
 	while (gap > 0)
