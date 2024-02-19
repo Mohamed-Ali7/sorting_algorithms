a408d71d7b13a2dcffb74c622e44f6646c2eb98b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-19 16:08:57 +0200

    Updating 105-radix_sort.c file

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -13,43 +13,44 @@
 
 void radix_sort(int *array, size_t size)
 {
-	int max_digits = 0, temp_max, max_number = 0;
-	int divider = 1, mod = 10, *count, *temp_array;
+	int max_number = 0, temp_element, new_index;
+	int divider = 1, *count, *temp_array;
 	size_t i;
-	int temp_element, new_index;
 
 	if (!array || size < 2)
 		return;
 	temp_array = malloc(sizeof(int) * size);
+	if (!temp_array)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		if (max_number < array[i])
 			max_number = array[i];
 	}
-	for (temp_max = max_number; temp_max != 0; max_digits++)
-		temp_max = temp_max / 10;
+
 	count = malloc(sizeof(int) * 10);
-	while (max_digits > 0)
+	if (!count)
+		return;
+	for (divider =  1; max_number / divider > 0; divider *= 10)
 	{
 		for (i = 0; i < 10; i++)
 			count[i] = 0;
 		for (i = 0; i < size; i++)
 		{
 			temp_array[i] = array[i];
-			temp_element = (temp_array[i] % mod) / divider;
+			temp_element = (temp_array[i] / divider) % 10;
 			count[temp_element]++;
 		}
 		for (i = 1; i < 10; i++)
 			count[i] += count[i - 1];
 		for (i = size - 1; (int) i >= 0; i--)
 		{
-			temp_element = (temp_array[i] % mod) / divider;
+			temp_element = (temp_array[i] / divider) % 10;
 			new_index = --count[temp_element];
 			array[new_index] = temp_array[i];
 		}
 		print_array(array, size);
-		mod *= 10;
-		divider *= 10;
-		max_digits--;
 	}
+	free(temp_array);
+	free(count);
 }
